4a4dbcc89e9e34622ac8bc7b20beb0fcf1b66be6
Jay Hayes <ur@iamvery.com>
2016-02-04 18:16:44 -0600

    Shamelessly borrow lessons from another project
    * https://github.com/dojo-toulouse/elixir-koans

---
+++ about_lists.exs
@@ -0,0 +1,44 @@
+#!/usr/bin/env elixir
+
+ExUnit.start
+
+defmodule About_Lists do
+    use ExUnit.Case
+    use Koans
+
+    think "Create your first list" do
+        a_list = __?
+        assert is_list(a_list)
+    end
+
+    think "Getting list length is a kernel feature" do
+        a_list = [1, 2, 3]
+        assert length(a_list) == __?
+    end
+
+    think "Elixir provide special operator to concatenate lists" do
+        a_list = [1, 2]
+        assert a_list ++ [3] == __?
+    end
+
+    think "Elixir provide special operator to remove element from list" do
+        a_list = [1, 2, 3]
+        assert a_list -- [2] == __?
+    end
+
+    think "Only first element is removed with truncate operator" do
+        a_list = [:foo, :bar, :foo]
+        assert a_list -- [:foo] == __?
+    end
+
+    think "Truncate operator do nothing when element not in list" do
+        a_list = [:foo, :bar]
+        assert_? a_list -- [:baz] == [:foo, :bar]
+    end
+
+    think "The in operator test if element is present inner an enum" do
+        a_list = [:foo, :bar]
+        assert_? :bar in a_list
+    end
+
+end
---
+++ about_testing.exs
@@ -0,0 +1,40 @@
+#!/usr/bin/env elixir
+
+# This should be replaced with the correct code to start the ExUnit server
+Koans.About_testing.start_exUnit!
+
+defmodule About_testing do
+    use FakeUnit.Case
+    use Koans.About_testing
+
+    think "We shall contemplate truth by testing reality, via asserts." do
+        assert __?
+    end
+
+    think "When reality lies, we shall refute truth" do
+        refute __?
+    end
+
+    think "Enlightenment may be more easily achieved with appropriate messages." do
+        assert __?, "This should be true -- Please fix this"
+    end
+
+    think "To understand reality, we must compare our expectations against reality." do
+        expected_value = __?
+        actual_value = 1 + 1
+
+        assert expected_value == actual_value
+    end
+
+    think "Assertions are smart" do
+        is_1_equal_2? = fn -> assert 1 == 2 end
+        is_1_greater_than_2? = fn -> assert 1 > 2 end
+
+        message = "Assertion with " <> __? <> " failed"
+        assert_raise ExUnit.AssertionError, message, is_1_equal_2?
+
+        message = "Assertion with " <> __? <> " failed"
+        assert_raise ExUnit.AssertionError, message, is_1_greater_than_2?
+    end
+end
+
