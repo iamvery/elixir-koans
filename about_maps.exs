dd2ea6e6ef81417a6a97c89ccab5931cdb825c37
Jay Hayes <ur@iamvery.com>
2016-04-14 18:27:59 -0500

    Update descriptions in tuple lesson

--- about_maps.exs
+++ about_maps.exs
@@ -1,24 +1,24 @@
 defmodule AboutMaps do
   use Koans
 
-  think "maps are a key-value store" do
+  think "Maps are a key-value store" do
     map = %{name: "Ryan", age: 27}
     assert map[:name] == __?
     assert map[:likes] == __?
   end
 
-  think "keys can be more than atoms" do
+  think "Keys can be any value" do
     map = %{"name" => "Ryan"}
     assert map["name"] == __?
   end
 
-  think "there is more than one way to access a map" do
+  think "There is more than one way to access a map" do
     map = %{name: "Ryan", age: 27}
     assert Map.get(map, :name) == __?
     assert Map.get(map, :likes, "programming") == __?
   end
 
-  think "you can ask a map about its keys" do
+  think "You can ask a map about its keys" do
     map = %{name: "Ryan", age: 27}
     assert Map.keys(map) == __?
     # Further meditation:
@@ -30,13 +30,13 @@ defmodule AboutMaps do
     assert Map.values(map) == __?
   end
 
-  think "you can ask a map if it has a key" do
+  think "You can ask a map if it has a key" do
     map = %{name: "Ryan", age: 27}
     assert Map.has_key?(map, :name) == __?
     assert Map.has_key?(map, :likes) == __?
   end
 
-  think "fetching a key works if the key exists..." do
+  think "Fetching a key works if the key exists..." do
     map = %{name: "Ryan", age: 27}
     assert Map.fetch(map, :name) == __?
   end
@@ -46,62 +46,62 @@ defmodule AboutMaps do
     assert Map.fetch(map, :likes) == __?
   end
 
-  think "you can also pop a key" do
+  think "You can also pop a key" do
     map = %{amount: 10}
     {value, map} = Map.pop(map, :amount)
     assert value == __?
     assert map == __?
   end
 
-  think "popping a non-existant key" do
+  think "Popping a non-existant key" do
     map = %{amount: 10}
     { value, map } = Map.pop(map, :age)
     assert value == __?
     assert map == __?
   end
 
-  think "merging together two maps" do
+  think "Merging together two maps" do
     map_1 = %{name: "Ryan", age: 27}
     map_2 = %{name: "Ryan B.", likes: "programming"}
     assert Map.merge(map_1, map_2) == __?
   end
 
-  think "merging together two maps and doing math on their values" do
+  think "Merging together two maps and doing something with their values" do
     map_1 = %{today: 10, yesterday: 20}
     map_2 = %{today: 20, yesterday: 5}
 
     assert Map.merge(map_1, map_2, fn (_k, v1, v2) -> v1 + v2 end) == __?
   end
 
-  think "dropping a key" do
+  think "Dropping keys" do
     map = %{name: "Ryan", age: 27}
     assert Map.drop(map, [:age]) == __?
   end
 
-  think "there's more than one way to remove a key" do
+  think "There's more than one way to remove a key" do
     map = %{name: "Ryan", age: 27}
     assert Map.delete(map, :age) == __?
   end
 
-  think "changing a key in a map" do
+  think "Changing a value" do
     map = %{name: "Ryan", age: 27}
     assert Map.put(map, :name, "Ryan B.") == __?
   end
 
-  think "putting a new key into a map" do
+  think "Adding a new pair that doesn't exist" do
     map = %{name: "Ryan", age: 27}
     assert Map.put_new(map, :likes, "Programming") == __?
     # Distinctly different from Map.put/3
     assert Map.put_new(map, :name, "Ryan B.") == __?
   end
 
-  think "updating a key in a map" do
+  think "Updating a key with a function" do
     map = %{amount: 10}
     assert Map.update(map, :amount, 0, fn (x) -> x + 1 end) == __?
     assert Map.update(map, :other_amount, 10, fn (x) -> x + 1 end) == __?
   end
 
-  think "adding keys that don't exist may not be desirable" do
+  think "Updating keys that don't exist may not be desirable" do
     map = %{amount: 10}
     assert Map.update!(map, :amount, fn (x) -> x + 1 end) == __?
     assert_raise __?, fn -> Map.update!(map, :other_amount, fn (x) -> x + 1 end) end
