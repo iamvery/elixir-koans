92652850a7bf3c5af9ef685167c2d17b9aa5f6cc
Jay Hayes <ur@iamvery.com>
2016-04-14 20:11:12 -0500

    Rework the matching lesson

--- about_matching.exs
+++ about_matching.exs
@@ -1,46 +1,51 @@
 defmodule AboutMatching do
   use Koans
 
-  think "It's like affecting a variable" do
+  think "The match operator binds values on the right to variables on the left" do
     a_variable = 1
     assert a_variable == __?
   end
 
-  think "The real operation is named matching" do
+  think "It matches patterns in data structures" do
     [a, b] = [1, 2]
+    [c, d, e] = [4, 5, 6]
 
-    assert a  == __? and b == __?
+    assert a == __?
+    assert b == __?
+    assert c == __?
+    assert d == __?
+    assert e == __?
   end
 
-  think "Matching three variables" do
-    [a, b, c] = [1, 2, 3]
-
-    assert a  == __? and b == __? and c == __?
-  end
-
-  think "Matching one variable and drop a value" do
+  think "You can ignore values in the match" do
     [a, _] = ["Keep me", "Drop me"]
 
     assert a  == __?
   end
 
-  think "Error matching" do
-    assert_raise MatchError, fn -> [ a , a ] = [ 1 , __? ] end
+  think "The sides must match" do
+    [a, a] = [1, __?]
+
+    assert a == __?
   end
 
-  think "Matching an array inner an array" do
-    [ a ] = [ ["Hello", "World !"] ]
+  think "It raises an error when the match fails" do
+    assert_raise MatchError, fn -> [a , a] = [1 , __?] end
+  end
+
+  think "Matching a list inside a list" do
+    [a] = [["Hello", "World !"]]
 
     assert a  == __?
   end
 
-  think "matching a tuple" do
-    { :ok, result } = { :ok, 12 }
+  think "Matching a tuple" do
+    {:ok, result} = {:ok, 12}
     assert result == __?
   end
 
-  think "heads and tails" do
-    [ head | tail ] = [1, 2, 3]
+  think "Matching the head and tail of a list" do
+    [head|tail] = [1, 2, 3]
     assert head == __?
     assert tail == __?
   end
