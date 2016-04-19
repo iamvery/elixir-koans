5c0fec2a4712ab5101267d80fac25a9b912388cf
Nathan Long <him@nathanmlong.com>
2016-04-19 06:56:29 -0400

    Dot syntax for maps is stricter

--- about_atoms.exs
+++ about_atoms.exs
@@ -15,13 +15,22 @@ defmodule AboutAtoms do
     assert __? == list[:name]
   end
 
-  think "Atom keys enable a different access syntax" do
+  think "Atom keys may be accessed with dot syntax" do
     map = %{name: "Jay"}
     assert map.name == __?
 
     map = %{"name" => "Jay"}
     attempt = fn -> __? end
     assert_raise KeyError, "key :name not found in: %{\"name\" => \"Jay\"}", attempt
+  end
+
+  think "Dot syntax is stricter than access with brackets" do
+    map = %{name: "Jay"}
+    bracket_access = fn -> map[:age] end
+    dot_access     = fn -> map.age end
+
+    assert bracket_access.() == __?
+    assert_raise __?, "key :age not found in: %{name: \"Jay\"}", dot_access
   end
 
   think "It is surprising to find out that booleans are atoms" do
