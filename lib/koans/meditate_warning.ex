87f38ea502b7d752c10458771b38f753c4cde723
Jay Hayes <ur@iamvery.com>
2016-04-20 18:42:53 -0500

    Extract private function to format exception message

--- lib/koans/meditate_warning.ex
+++ lib/koans/meditate_warning.ex
@@ -2,13 +2,16 @@ defmodule Koans.MeditateWarning do
   defexception [:message]
   def message(exception) do
     location = System.stacktrace |> Enum.at(1) |> elem(3)
-    formatted_message = IO.ANSI.format(
+    "#{format(exception, location)}"
+  end
+
+  defp format(exception, location) do
+    IO.ANSI.format(
     [
       :magenta, :bright, "Please meditate: ",
       :blue, :normal, exception.message,
       :yellow, " (#{location[:file]}:#{location[:line]})"
     ]
     )
-    "#{formatted_message}"
   end
 end
