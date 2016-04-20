40da61cc66f5dc6e11548968ce354f67a290d7d4
Jay Hayes <ur@iamvery.com>
2016-04-20 18:43:22 -0500

    whitespace

--- lib/koans/meditate_warning.ex
+++ lib/koans/meditate_warning.ex
@@ -6,12 +6,10 @@ defmodule Koans.MeditateWarning do
   end
 
   defp format(exception, location) do
-    IO.ANSI.format(
-    [
+    IO.ANSI.format([
       :magenta, :bright, "Please meditate: ",
       :blue, :normal, exception.message,
       :yellow, " (#{location[:file]}:#{location[:line]})"
-    ]
-    )
+    ])
   end
 end
