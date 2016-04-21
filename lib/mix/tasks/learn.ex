a7efaee9a74c5f697f73e647b36efb7e8b26aa8e
Jay Hayes <ur@iamvery.com>
2016-04-20 20:27:33 -0500

    Extract runner module

--- lib/mix/tasks/learn.ex
+++ lib/mix/tasks/learn.ex
@@ -6,7 +6,7 @@ defmodule Mix.Tasks.Learn do
   @shortdoc "Learn Elixir through meditation"
 
   def run(_) do
-    System.at_exit(fn 0 -> Koans.run end)
+    System.at_exit(fn 0 -> Koans.Runner.run end)
     Koans.Lessons.load
   end
 end
