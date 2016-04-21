3144b7366f1ca323b49254f0f964e21c86fd6c03
Jay Hayes <ur@iamvery.com>
2016-04-21 17:09:00 -0500

    Answerer injects missing values

--- lib/koans/answerer.ex
+++ lib/koans/answerer.ex
@@ -1,2 +1,5 @@
 defmodule Koans.Answerer do
+  def inject({:__?, _, _}, [answer|rest]) do
+    {answer, rest}
+  end
 end
--- test/koans/answerer_test.exs
+++ test/koans/answerer_test.exs
@@ -1,3 +1,12 @@
 defmodule KoansAnswererTest do
   use ExUnit.Case
+
+  import Koans.Answerer
+
+  test "value injected" do
+    value_is_missing = quote do: __?
+    {with_value, []} = inject(value_is_missing, [:lol])
+
+    assert with_value == quote(do: :lol)
+  end
 end
