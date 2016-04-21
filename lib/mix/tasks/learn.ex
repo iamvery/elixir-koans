ec57d45f4289f32658c0718add2d6fcf5adc383f
Jay Hayes <ur@iamvery.com>
2016-04-20 20:30:27 -0500

    Ensure app is started for the learn task

--- lib/mix/tasks/learn.ex
+++ lib/mix/tasks/learn.ex
@@ -6,6 +6,7 @@ defmodule Mix.Tasks.Learn do
   @shortdoc "Learn Elixir through meditation"
 
   def run(_) do
+    Application.ensure_all_started(:koans)
     System.at_exit(fn 0 -> Koans.Runner.run end)
     Koans.Lessons.load
   end
