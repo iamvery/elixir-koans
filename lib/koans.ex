edd0b9b9ed4d2aa47c96cd54bf8c889ed366cdc0
Jay Hayes <ur@iamvery.com>
2016-04-20 20:29:59 -0500

    Implement application callback module

--- lib/koans.ex
+++ lib/koans.ex
@@ -1,4 +1,15 @@
 defmodule Koans do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+    ]
+
+    opts = [strategy: :one_for_one, name: Koans.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 
   defmacro __using__([]) do
     quote do
