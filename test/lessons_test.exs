6333c8ca0450ae462834bf375741e8a0c2f422f6
Jay Hayes <ur@iamvery.com>
2016-04-22 10:33:28 -0500

    Remove lessons test
    Compilation will be tested by running answered koans.

--- test/lessons_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LessonsTest do
-  use ExUnit.Case
-
-  test "lessons compile" do
-    Koans.Lessons.load
-    # no assertions needed here, compilation failure would cause a test failure
-  end
-end
