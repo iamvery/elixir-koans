0236a5f824ac59cd74d8480b99e55446a7f33935
Jay Hayes <ur@iamvery.com>
2016-04-22 10:34:13 -0500

    Convert attribute to function
    We'll end up accessing this collection at runtime.

--- lib/koans/lessons.ex
+++ lib/koans/lessons.ex
@@ -1,23 +1,25 @@
 defmodule Koans.Lessons do
-  @lessons [
-    "about_asserts.exs",
-    "about_numbers_and_booleans.exs",
-    "about_strings.exs",
-    "about_atoms.exs",
-    "about_lists.exs",
-    "about_matching.exs",
-    "about_sigils.exs",
-    "about_enums.exs",
-    "about_tuples.exs",
-    "about_maps.exs",
-    "about_lists_and_maps.exs",
-    "about_anonymous_functions.exs",
-    "about_named_functions.exs",
-    "about_binaries.exs",
-    "about_processes.exs",
-  ]
+  def all do
+    [
+      "about_asserts.exs",
+      "about_numbers_and_booleans.exs",
+      "about_strings.exs",
+      "about_atoms.exs",
+      "about_lists.exs",
+      "about_matching.exs",
+      "about_sigils.exs",
+      "about_enums.exs",
+      "about_tuples.exs",
+      "about_maps.exs",
+      "about_lists_and_maps.exs",
+      "about_anonymous_functions.exs",
+      "about_named_functions.exs",
+      "about_binaries.exs",
+      "about_processes.exs",
+    ]
+  end
 
   def load do
-    @lessons |> Enum.each(&Code.load_file/1)
+    all |> Enum.each(&Code.load_file/1)
   end
 end
